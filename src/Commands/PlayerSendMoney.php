7dd19aff3bbcaa3b40e5fe05edf7eb5fb48bb2c2
IcyEndymion004 <63156236+IcyEndymion004@users.noreply.github.com>
2022-03-14 14:56:51 -0500

    Update PlayerSendMoney.php

--- src/Commands/PlayerSendMoney.php
+++ src/Commands/PlayerSendMoney.php
@@ -40,13 +40,11 @@ final class PlayerSendMoney extends Command implements PluginOwned {
             $sender->sendMessage($prefix . "§cPlease Specify a amount");
         }
         $api = $this->getAPI();
-        $money = $api->getMoney($args[0]);
-        $money = strval($money);
         $yourmoney = $api->getMoney($sender->getName());
         if($yourmoney > $args[1]){
         $api->removeMoney($sender->getName(), intval($args[1]));
         $api->addMoney($args[0], intval($args[1]));
-        $sender->sendMessage($prefix . "§aYou sent" . $args[0] . " " .$money . "$ Money");
+        $sender->sendMessage($prefix . "§aYou sent" . $args[0] . " " . $args[1] . "$ Money");
         }else{
         $sender->sendMessage($prefix . "§c You do not have that much money");
         }
